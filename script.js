c2976c94652a15fff14f3e4ee3a7419fc5fede8e
Taras Shevchenko <77995009+TaraShevchenko@users.noreply.github.com>
2023-06-29 00:09:31 +0300

    Adaptive intro page.

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('cross');
     menuList.classList.toggle('active');
 });
+
+const images = document.querySelectorAll('.svg');
+
+document.addEventListener('mousemove', moveImages);
+
+function moveImages(e) {
+    const mouseX = e.clientX; // Получаем текущую позицию курсора по оси X
+    const mouseY = e.clientY; // Получаем текущую позицию курсора по оси Y
+
+    images.forEach(image => {
+        const rect = image.getBoundingClientRect();
+        const imageX = rect.left + rect.width / 2; // Получаем центр изображения по оси X
+        const imageY = rect.top + rect.height / 2; // Получаем центр изображения по оси Y
+
+        const deltaX = (mouseX - imageX) * 0.03; // Задаем множитель, определяющий скорость движения
+        const deltaY = (mouseY - imageY) * 0.03;
+
+        // Обновляем позицию изображения с учетом параллакс-эффекта
+        image.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    });
+}
+
+window.addEventListener('mouseout', resetImages);
+
+function resetImages() {
+    images.forEach(image => {
+        image.style.transform = 'translate(0, 0)'; // Возвращаем изображения на их изначальные позиции
+    });
+}
+
+
+
+
+
